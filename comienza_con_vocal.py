0dfd8706ac0175ab59e5b63ab6853ec291ad33a2
angelicalbarradiazz-stack <angelicalbarradiazz@gmail.com>
2025-09-19 03:00:39 -0600

    aaaaaa

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -1,15 +1,42 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
+
+Determinar si una palabra comienza con una vocal.
+    Args:
+        Palabra (str): Palabra que se evaluará
+
+    Returns:
+        str: Mensaje indicando si la palabra comienza con vocal o no.
+
+
+
+
+
 """
 
 # Declaraciones
-CONSTANTE = valor
+#CONSTANTE = valor
+Palabra= str(input("Escríbe tu palabra "))
+vocalornot= Palabra[0]
+mensajesivocal= ((Palabra)+(" comienza con vocal"))
+mensajesinvocal= ((Palabra)+(" no comienza con vocal"))
+
 
 # Entradas
-entrada = input()
+#Palabra= str(input("Escríbe tu palabra"))
+
 
 # Proceso
+if(vocalornot == 'a' or
+    vocalornot == 'e' or
+    vocalornot == 'i' or
+    vocalornot == 'o' or
+    vocalornot == 'u'):
+    salida=mensajesivocal
+else:
+    salida=mensajesinvocal
 
+print(salida)
 
 # Salidas
-print(salida)
+
